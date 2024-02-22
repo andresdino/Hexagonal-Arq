590399b776fdf5c9a612ee89d11fca4c1b3f01bc
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 14:01:26 -0500

    Feat/: Removing notations

--- src/main/java/com/example/demo/infrastructure/repository/JpaCourseRepository.java
+++ src/main/java/com/example/demo/infrastructure/repository/JpaCourseRepository.java
@@ -2,9 +2,7 @@ package com.example.demo.infrastructure.repository;
 
 import com.example.demo.infrastructure.entities.CourseEntity;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface JpaCourseRepository extends CrudRepository<CourseEntity, Long> {
 
 }
--- src/main/java/com/example/demo/infrastructure/repository/JpaStudentRepository.java
+++ src/main/java/com/example/demo/infrastructure/repository/JpaStudentRepository.java
@@ -2,9 +2,7 @@ package com.example.demo.infrastructure.repository;
 
 import com.example.demo.infrastructure.entities.StudentEntity;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface JpaStudentRepository extends CrudRepository<StudentEntity, Long> {
 
 }
