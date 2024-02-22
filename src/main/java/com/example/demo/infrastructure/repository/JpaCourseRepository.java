4923544c53faa647426cfe15093b759d0c484243
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 15:49:34 -0500

    Feat/: Updating Repository

--- src/main/java/com/example/demo/infrastructure/repository/JpaCourseRepository.java
+++ src/main/java/com/example/demo/infrastructure/repository/JpaCourseRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.infrastructure.repository;
 
 import com.example.demo.infrastructure.entities.CourseEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaCourseRepository extends CrudRepository<CourseEntity, Long> {
 
 }
