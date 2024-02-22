081b2780f19143c34f2e7085007d3cb1db67df73
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 09:24:25 -0500

    Feat/: Updating JpaRepository Adapter

--- src/main/java/com/example/demo/infrastructure/adapter/JpaCourseRepositoryAdapter.java
+++ src/main/java/com/example/demo/infrastructure/adapter/JpaCourseRepositoryAdapter.java
@@ -4,6 +4,7 @@ import com.example.demo.domain.models.Course;
 import com.example.demo.domain.ports.out.CourseRepositoryPort;
 import com.example.demo.infrastructure.repository.JpaCourseRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class JpaCourseRepositoryAdapter implements CourseRepositoryPort {
@@ -15,67 +16,27 @@ public class JpaCourseRepositoryAdapter implements CourseRepositoryPort {
     }
 
     @Override
+    public Course save(Course course) {
+        return null;
+    }
+
+    @Override
+    public Optional<Course> findById(Long courseId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Course> findAll() {
+        return null;
+    }
+
+    @Override
     public Optional<Course> update(Course course) {
         return Optional.empty();
     }
 
     @Override
-    public <S extends Course> S save(S entity) {
-        return null;
-    }
-
-    @Override
-    public <S extends Course> Iterable<S> saveAll(Iterable<S> entities) {
-        return null;
-    }
-
-    @Override
-    public Optional<Course> findById(Long aLong) {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean existsById(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public Iterable<Course> findAll() {
-        return null;
-    }
-
-    @Override
-    public Iterable<Course> findAllById(Iterable<Long> longs) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(Long aLong) {
-
-    }
-
-    @Override
-    public void delete(Course entity) {
-
-    }
-
-    @Override
-    public void deleteAllById(Iterable<? extends Long> longs) {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends Course> entities) {
-
-    }
-
-    @Override
-    public void deleteAll() {
+    public void deleteById(Long courseId) {
 
     }
 }
--- src/main/java/com/example/demo/infrastructure/adapter/JpaStudentRepositoryAdapter.java
+++ src/main/java/com/example/demo/infrastructure/adapter/JpaStudentRepositoryAdapter.java
@@ -4,6 +4,7 @@ import com.example.demo.domain.models.Student;
 import com.example.demo.domain.ports.out.StudentRepositoryPort;
 import com.example.demo.infrastructure.repository.JpaStudentRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class JpaStudentRepositoryAdapter implements StudentRepositoryPort {
@@ -14,68 +15,29 @@ public class JpaStudentRepositoryAdapter implements StudentRepositoryPort {
         this.jpaStudentRepository = jpaStudentRepository;
     }
 
+
+    @Override
+    public Student save(Student student) {
+        return null;
+    }
+
+    @Override
+    public Optional<Student> findById(Long studentId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+
     @Override
     public Optional<Student> update(Student student) {
         return Optional.empty();
     }
 
     @Override
-    public <S extends Student> S save(S entity) {
-        return null;
-    }
-
-    @Override
-    public <S extends Student> Iterable<S> saveAll(Iterable<S> entities) {
-        return null;
-    }
-
-    @Override
-    public Optional<Student> findById(Long aLong) {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean existsById(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public Iterable<Student> findAll() {
-        return null;
-    }
-
-    @Override
-    public Iterable<Student> findAllById(Iterable<Long> longs) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(Long aLong) {
-
-    }
-
-    @Override
-    public void delete(Student entity) {
-
-    }
-
-    @Override
-    public void deleteAllById(Iterable<? extends Long> longs) {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends Student> entities) {
-
-    }
-
-    @Override
-    public void deleteAll() {
+    public void deleteById(Long studentId) {
 
     }
 }
