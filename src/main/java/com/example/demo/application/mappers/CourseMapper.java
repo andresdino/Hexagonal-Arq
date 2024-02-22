541a0c00808d208c7987220150fdde5937a26099
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 09:11:06 -0500

    Feat/: Mappers Student and Course

--- src/main/java/com/example/demo/application/mappers/CourseMapper.java
+++ src/main/java/com/example/demo/application/mappers/CourseMapper.java
@@ -5,32 +5,49 @@ import com.example.demo.application.dto.response.CourseResponseDto;
 import com.example.demo.application.dto.response.StudentResponseDto;
 import com.example.demo.domain.models.Course;
 import com.example.demo.domain.models.Student;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 
-import java.lang.reflect.Type;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CourseMapper {
 
-    private static final ModelMapper modelMapper = new ModelMapper();
+    public static Course CourseDtoToCourse(CourseRequestDto courseRequestDto) {
+        Course course = new Course();
+        course.setId(courseRequestDto.getId());
+        course.setName(courseRequestDto.getName());
+        course.setTeacher(courseRequestDto.getTeacher());
 
-    public static Course mapToCourse(CourseRequestDto courseRequestDto) {
-        return modelMapper.map(courseRequestDto, Course.class);
+        List<Student> students = courseRequestDto.getStudents().stream()
+                .map(studentDto -> {
+                    Student student = new Student();
+                    student.setId(studentDto.getId());
+                    student.setName(studentDto.getName());
+                    student.setCourseId(studentDto.getCourseId());
+                    return student;
+                })
+                .collect(Collectors.toList());
+
+        course.setStudents(students);
+        return course;
     }
 
-    public static CourseResponseDto mapToCourseResponseDto(Course course) {
-        return modelMapper.map(course, CourseResponseDto.class);
-    }
+    public static CourseResponseDto CourseToCourseDto(Course course) {
+        CourseResponseDto courseResponseDto = new CourseResponseDto();
+        courseResponseDto.setId(course.getId());
+        courseResponseDto.setName(course.getName());
+        courseResponseDto.setTeacher(course.getTeacher());
 
-    public static List<CourseResponseDto> mapToCourseResponseDtoList(List<Course> courses) {
-        Type listType = new TypeToken<List<CourseResponseDto>>(){}.getType();
-        return modelMapper.map(courses, listType);
-    }
+        List<StudentResponseDto> studentResponseDtos = course.getStudents().stream()
+                .map(student -> {
+                    StudentResponseDto studentResponseDto = new StudentResponseDto();
+                    studentResponseDto.setId(student.getId());
+                    studentResponseDto.setName(student.getName());
+                    studentResponseDto.setCourseId(student.getCourseId());
+                    return studentResponseDto;
+                })
+                .collect(Collectors.toList());
 
-    public static List<StudentResponseDto> mapToStudentResponseDtoList(List<Student> students) {
-        Type listType = new TypeToken<List<StudentResponseDto>>(){}.getType();
-        return modelMapper.map(students, listType);
+        courseResponseDto.setStudents(studentResponseDtos);
+        return courseResponseDto;
     }
 }
--- src/main/java/com/example/demo/application/mappers/StudentMapper.java
+++ src/main/java/com/example/demo/application/mappers/StudentMapper.java
@@ -23,4 +23,5 @@ public class StudentMapper {
 
         return studentResponseDto;
     }
+
 }
