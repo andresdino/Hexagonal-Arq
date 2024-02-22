420aa26e5df3968a770c60c95b2301fa4c2c1fb2
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 16:53:03 -0500

    Feat/: Making bean config

---
+++ src/main/java/com/example/demo/infrastructure/Config/BeanCourseConfiguration.java
@@ -0,0 +1,16 @@
+//package com.example.demo.infrastructure.Config;
+//
+//import com.example.demo.application.services.CourseService;
+//import com.example.demo.application.usecases.CourseUseCaseImpl;
+//import com.example.demo.domain.ports.in.CourseUseCase;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class BeanCourseConfiguration {
+//
+//    @Bean
+//    CourseService courseBeanService(final CourseUseCaseImpl courseUseCase){
+//        return new CourseService();
+//    }
+//}
---
+++ src/main/java/com/example/demo/infrastructure/Config/BeanStudentConfiguration.java
@@ -0,0 +1,16 @@
+//package com.example.demo.infrastructure.Config;
+//
+//import com.example.demo.application.services.StudentService;
+//import com.example.demo.application.usecases.StudentUseCaseImpl;
+//import com.example.demo.domain.ports.in.StudentUseCase;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class BeanStudentConfiguration {
+//
+//    @Bean
+//    StudentService studentBeanService(final StudentUseCaseImpl studentUseCase){
+//        return new StudentService(studentUseCase);
+//    }
+//}
