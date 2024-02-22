f41e4ccc7642d5d1d28290918cb932ddfbb0cbe6
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 16:53:44 -0500

    Feat/: Making Course Mapper

--- src/main/java/com/example/demo/infrastructure/rest/mapper/CourseMapper.java
+++ src/main/java/com/example/demo/infrastructure/rest/mapper/CourseMapper.java
@@ -1,21 +1,20 @@
-package com.example.demo.infrastructure.mapper;
+package com.example.demo.infrastructure.rest.mapper;
 
 import com.example.demo.domain.models.Course;
 import com.example.demo.infrastructure.entities.CourseEntity;
-import lombok.Getter;
-import lombok.Setter;
 import org.modelmapper.ModelMapper;
 
 public class CourseMapper {
 
     private static final ModelMapper modelMapper = new ModelMapper();
 
-    public static CourseEntity fromDomainModel(Course course) {
+    public static CourseEntity modelToEntity(Course course) {
         return modelMapper.map(course, CourseEntity.class);
     }
 
-    public static Course toDomainModel(CourseEntity courseEntity) {
+    public static Course entityToModel(CourseEntity courseEntity) {
         return modelMapper.map(courseEntity, Course.class);
     }
+
 
 }
