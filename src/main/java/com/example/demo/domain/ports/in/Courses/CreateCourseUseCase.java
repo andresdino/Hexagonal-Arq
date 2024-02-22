ed594ae48198c0a816da33c7f93e5af33a4eab13
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 13:59:50 -0500

    Feat/: Optimizing imports

--- src/main/java/com/example/demo/domain/ports/in/Courses/CreateCourseUseCase.java
+++ src/main/java/com/example/demo/domain/ports/in/Courses/CreateCourseUseCase.java
@@ -1,7 +1,5 @@
 package com.example.demo.domain.ports.in.Courses;
 
-import com.example.demo.application.dto.request.CourseRequestDto;
-import com.example.demo.application.dto.response.CourseResponseDto;
 import com.example.demo.domain.models.Course;
 
 public interface CreateCourseUseCase {
