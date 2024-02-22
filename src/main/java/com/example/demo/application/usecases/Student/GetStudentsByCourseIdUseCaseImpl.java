764cc6370f004d47ab8008ffa20887e2cf73370d
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 09:23:50 -0500

    Feat/: Updating GetStudentsByCourseIdUseCaseImpl

--- src/main/java/com/example/demo/application/usecases/Student/GetStudentsByCourseIdUseCaseImpl.java
+++ src/main/java/com/example/demo/application/usecases/Student/GetStudentsByCourseIdUseCaseImpl.java
@@ -2,20 +2,21 @@ package com.example.demo.application.usecases.Student;
 
 import com.example.demo.domain.models.Student;
 import com.example.demo.domain.ports.in.Students.GetStudentsByCourseIdUseCase;
+import com.example.demo.domain.ports.out.ExternalServicePort;
 import com.example.demo.domain.ports.out.StudentRepositoryPort;
 
 import java.util.List;
 
 public class GetStudentsByCourseIdUseCaseImpl implements GetStudentsByCourseIdUseCase {
 
-    private final StudentRepositoryPort studentRepositoryPort;
+    private final ExternalServicePort externalServicePort;
 
-    public GetStudentsByCourseIdUseCaseImpl(StudentRepositoryPort studentRepositoryPort) {
-        this.studentRepositoryPort = studentRepositoryPort;
+    public GetStudentsByCourseIdUseCaseImpl(ExternalServicePort externalServicePort) {
+        this.externalServicePort = externalServicePort;
     }
 
     @Override
     public List<Student> getStudentsByCourseId(Long courseId) {
-        return (List<Student>) studentRepositoryPort.findById(courseId).orElse(null);
+        return (List<Student>) externalServicePort.getStudentsByCourseId(courseId);
     }
 }
