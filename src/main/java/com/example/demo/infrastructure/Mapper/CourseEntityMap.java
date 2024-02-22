00fd69f67c9752e7419b9ce0aa14e181e0bb6ae4
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 11:07:36 -0500

    Feat/: Creating EntityMap

---
+++ src/main/java/com/example/demo/infrastructure/Mapper/CourseEntityMap.java
@@ -0,0 +1,4 @@
+package com.example.demo.infrastructure.Mapper;
+
+public class CourseEntityMap {
+}
---
+++ src/main/java/com/example/demo/infrastructure/Mapper/StudentEntityMap.java
@@ -0,0 +1,14 @@
+package com.example.demo.infrastructure.Mapper;
+
+import com.example.demo.domain.models.Student;
+import com.example.demo.infrastructure.entities.StudentEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudentEntityMap {
+
+
+
+}
