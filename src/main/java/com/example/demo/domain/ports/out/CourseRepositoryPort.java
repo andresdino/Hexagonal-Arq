9565983be60884df6b7ee5f1a969424236374e44
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 09:23:21 -0500

    Feat/: Updating Repositories Ports

--- src/main/java/com/example/demo/domain/ports/out/CourseRepositoryPort.java
+++ src/main/java/com/example/demo/domain/ports/out/CourseRepositoryPort.java
@@ -3,10 +3,15 @@ package com.example.demo.domain.ports.out;
 import com.example.demo.domain.models.Course;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface CourseRepositoryPort extends CrudRepository<Course, Long> {
+public interface CourseRepositoryPort {
 
+    Course save (Course course);
+    Optional<Course> findById(Long courseId);
+    List<Course> findAll();
     Optional<Course> update(Course course);
+    void deleteById(Long courseId);
 
 }
