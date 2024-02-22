4bca705938f0964bb54904d45f9b6fd5036c1c63
Hector Sanchez <andres.sanchez@quind.io>
2024-02-22 13:55:52 -0500

    Feat/: updating RepositoryPorts

--- src/main/java/com/example/demo/domain/ports/out/CourseRepositoryPort.java
+++ src/main/java/com/example/demo/domain/ports/out/CourseRepositoryPort.java
@@ -1,7 +1,6 @@
 package com.example.demo.domain.ports.out;
 
 import com.example.demo.domain.models.Course;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/example/demo/domain/ports/out/StudentRepositoryPort.java
+++ src/main/java/com/example/demo/domain/ports/out/StudentRepositoryPort.java
@@ -1,8 +1,6 @@
 package com.example.demo.domain.ports.out;
 
-import com.example.demo.domain.models.Course;
 import com.example.demo.domain.models.Student;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
