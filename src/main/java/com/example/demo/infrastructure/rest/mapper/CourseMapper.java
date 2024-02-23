c3574f8fed97f914ccaa6205b9c7c31d3ad0fb7c
Hector Sanchez <andres.sanchez@quind.io>
2024-02-23 08:19:55 -0500

    Feat/: Updating Mappers

--- src/main/java/com/example/demo/infrastructure/rest/mapper/CourseMapper.java
+++ src/main/java/com/example/demo/infrastructure/rest/mapper/CourseMapper.java
@@ -2,7 +2,11 @@ package com.example.demo.infrastructure.rest.mapper;
 
 import com.example.demo.domain.models.Course;
 import com.example.demo.infrastructure.entities.CourseEntity;
+import com.example.demo.infrastructure.rest.dto.CourseRequestDto;
+import com.example.demo.infrastructure.rest.dto.CourseResponseDto;
 import org.modelmapper.ModelMapper;
+
+import java.util.stream.Collectors;
 
 public class CourseMapper {
 
@@ -15,5 +19,29 @@ public class CourseMapper {
     public static Course toDomainModel(CourseEntity courseEntity) {
         return modelMapper.map(courseEntity, Course.class);
     }
+
+    public static Course toEntity(CourseRequestDto courseRequestDto){
+        Course course = new Course();
+        course.setName(courseRequestDto.getName());
+        course.setTeacher(courseRequestDto.getTeacher());
+        return course;
+    }
+
+    public static CourseResponseDto toDto(Course course){
+        CourseResponseDto courseResponseDto = new CourseResponseDto();
+        courseResponseDto.setId(course.getId());
+        courseResponseDto.setName(course.getName());
+        courseResponseDto.setTeacher(course.getTeacher());
+        if (course.getStudents() != null){
+            courseResponseDto.setStudents(
+                    course.getStudents()
+                            .stream()
+                            .map(StudentMapper::toDto)
+                            .collect(Collectors.toList()));
+        }
+        return courseResponseDto;
+    }
+
+
 
 }
--- src/main/java/com/example/demo/infrastructure/rest/mapper/StudentMapper.java
+++ src/main/java/com/example/demo/infrastructure/rest/mapper/StudentMapper.java
@@ -2,6 +2,8 @@ package com.example.demo.infrastructure.rest.mapper;
 
 import com.example.demo.domain.models.Student;
 import com.example.demo.infrastructure.entities.StudentEntity;
+import com.example.demo.infrastructure.rest.dto.StudentRequestDto;
+import com.example.demo.infrastructure.rest.dto.StudentResponseDto;
 import org.modelmapper.ModelMapper;
 
 public class StudentMapper {
@@ -14,6 +16,21 @@ public class StudentMapper {
 
     public static Student toDomainModel(StudentEntity studentEntity){
         return modelMapper.map(studentEntity, Student.class);
+    }
+
+    public static Student toEntity(StudentRequestDto studentRequestDto) {
+        Student student = new Student();
+        student.setName(studentRequestDto.getName());
+        student.setCourse(studentRequestDto.getCourse());
+        return student;
+    }
+
+    public static StudentResponseDto toDto(Student student) {
+        StudentResponseDto studentResponseDto = new StudentResponseDto();
+        studentResponseDto.setId(student.getId());
+        studentResponseDto.setName(student.getName());
+        studentResponseDto.setCourse(student.getCourse());
+        return studentResponseDto;
     }
 
 }
